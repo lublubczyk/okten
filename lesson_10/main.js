aa3e3cab253a05c6f57a68d9206eb4ad021f4740
lublubczyk <lubczyk@ukr.net>
2023-09-20 00:17:03 +0300

    accomplish homework js lesson 10

--- lesson_10/main.js
+++ lesson_10/main.js
@@ -34,9 +34,14 @@ document.body.appendChild(divForUsersData);
 formForUserData.onsubmit = function (event) {
     event.preventDefault();
     const divUserData = document.createElement('div');
-    divUserData.innerText =
-        `Name: ${this.userName.value}; Password: ${this.userSurName.value}; Age: ${this.userAge.value}`;
-    divForUsersData.appendChild(divUserData);
+    if (this.userName.value && this.userSurName.value && +this.userAge.value >= 0) {
+        divUserData.innerText =
+            `Name: ${this.userName.value}; 
+             Password: ${this.userSurName.value}; 
+             Age: ${this.userAge.value}`;
+        divUserData.style.margin = '10px'
+        divForUsersData.appendChild(divUserData);
+    };
     this.reset();
 };
 
@@ -44,17 +49,19 @@ formForUserData.onsubmit = function (event) {
 //перезавантажені сторінки буде додавати до неї + 1
 
 const divForCounter = document.createElement('div');
-divForCounter.style.width = '100px';
-divForCounter.style.backgroundColor = 'blue';
-divForCounter.innerText = '0';
-divForCounter.style.color = 'yellow';
-divForCounter.style.textAlign = 'center';
-divForCounter.style.fontSize = '70px';
+const styleForCount = {
+    width: '100px', backgroundColor: 'blue', margin: '10px',
+    color: 'yellow', textAlign: 'center', fontSize: '50px'
+};
+Object.assign(divForCounter.style, styleForCount);
+
+let counter = '1'
+divForCounter.innerText = counter;
 
 document.body.appendChild(divForCounter);
-let counter = divForCounter.innerText;
+
 if (localStorage.getItem('counter') === null) {
-    localStorage.setItem('counter',counter.toString());
+    localStorage.setItem('counter',counter);
 } else {
     counter = +localStorage.getItem('counter');
     counter++;
@@ -114,26 +121,157 @@ function create10Elem(first) {
     const count = first + 10;
     for (first; first < count; first++) {
         const divForElem = document.createElement('div');
+        divForElem.style.margin = '5px';
         divForElem.innerText = dataArr[first];
         divFor10Elem.appendChild(divForElem);
     };
 };
 
-//- Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, 
+//- Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript,
 //зробіть так, щоб при натисканні на кнопку зникав елемент з id = "text".
 
+const hidenElement = document.createElement('div');
+const hidenButton = document.createElement('button');
+const divForButton = document.createElement('div');
+
+hidenButton.innerText = 'Hide';
+hidenElement.setAttribute('id', 'text');
+
+const styleVal = {
+    element: { width: '300px', height: '200px', backgroundColor: '#FF7F50',margin: '10px' },
+    button: { fontSize: '30px', width: '100px', margin:'10px' }
+};
+
+Object.assign(hidenElement.style, styleVal.element);
+Object.assign(hidenButton.style, styleVal.button);
+
+divForButton.appendChild(hidenButton);
+document.body.append(divForButton, hidenElement);
+
+hidenButton.addEventListener('click', function () {
+    const hidElement = document.getElementById('text');
+    if (hidElement.style.display === 'none') {
+        hidElement.style.display = 'block';
+        this.innerText = 'Hide';
+    } else {
+        hidElement.style.display = 'none';
+        this.innerText = 'Show';
+    };
+});
 
 //- створити інпут який приймає вік людини та кнопку яка підтверджує дію.
-//При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, 
+//При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18,
 //та повідомити про це користувача
 
+const formForAge = document.createElement('form');
+const inputForFormAge = document.createElement('input');
+const buttonForFormAge = document.createElement('button');
+const atributeArray = [['type', 'number'], ['name', 'userAge'], ['placeholder', 'your age']];
+
+atributeArray.forEach(val => inputForFormAge.setAttribute(val[0], val[1]));
+formForAge.setAttribute('id', 'check');
+formForAge.setAttribute('action', '');
+buttonForFormAge.innerText = 'Check';
+
+formForAge.append(inputForFormAge, buttonForFormAge);
+document.body.appendChild(formForAge);
+
+const checkAge = document.querySelector('#check');
+const anver = document.createElement('h3');
+document.body.appendChild(anver);
+
+checkAge.onsubmit = function (event) {
+    event.preventDefault();
+    if (+this.userAge.value > 0) {
+        anver.innerText = +this.userAge.value > 17 ? 'Adult' : 'Baby';
+    } else anver.innerText = '';
+    this.reset();
+};
 
 //*** Створити 3 інпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 //(Додатковачастина для завдання)
 
-//*** (подібне було вище, але...будьте уважні в другій частині) створити сторінку з довільним блоком, 
+const inputForRow = document.createElement('input');
+const inputForColumn = document.createElement('input');
+const inputForText = document.createElement('input');
+const buttonForSet = document.createElement('button');
+const formForSetValue = document.createElement('form');
+buttonForSet.innerText = 'Set params';
+
+const attrObjSet = {
+    formForSet: [['action', ''], ['id', 'setSittings']],
+    inpRow: [['type', 'number'], ['name', 'userNumRow'], ['placeholder', 'set num of row']],
+    inpCol: [['type', 'number'], ['name', 'userNumCol'], ['placeholder', 'set num of col']],
+    inpText: [['type', 'text'], ['name', 'userText'], ['placeholder', 'set text']],
+};
+
+attrObjSet.formForSet.forEach(value => formForSetValue.setAttribute(value[0], value[1]));
+attrObjSet.inpRow.forEach(value => inputForRow.setAttribute(value[0], value[1]));
+attrObjSet.inpCol.forEach(value => inputForColumn.setAttribute(value[0], value[1]));
+attrObjSet.inpText.forEach(value => inputForText.setAttribute(value[0], value[1]));
+
+formForSetValue.append(inputForRow, inputForColumn, inputForText, buttonForSet);
+document.body.appendChild(formForSetValue);
+
+const getSettings = document.getElementById('setSittings');
+const divConteiner = document.createElement('div');
+document.body.appendChild(divConteiner);
+
+getSettings.onsubmit = function (event) {
+    event.preventDefault();
+    const rowNum = +this.userNumRow.value;
+    const colNum = +this.userNumCol.value;
+    const setText = this.userText.value;
+    if (rowNum > 0 && colNum > 0 && setText) {
+        const styleObj = {
+            width: 90 / colNum + '%',
+            float: 'left',
+            backgroundColor: '#B22222',
+            'margin-right': 5 / colNum + '%',
+            'margin-left': 5 / colNum + '%,',
+            'margin-top': '5px',
+            'margin-bottom': '5px',
+            fontSize: '20px',
+            textAlign: 'center',
+        };
+        divConteiner.innerHTML = '';
+        for (let i = 0; i < colNum * rowNum; i++) {
+            const elem = document.createElement('div');
+            Object.assign(elem.style, styleObj);
+            elem.innerText = setText;
+            divConteiner.appendChild(elem);
+        };
+    };
+    this.reset();
+};
+
+//*** (подібне було вище, але...будьте уважні в другій частині) створити сторінку з довільним блоком,
 //в середині якого є значення "100грн"
 //при перезавантаженні сторінки до значаення додається по 10грн, але !!!
 //зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
 // При перезавантаженні, яке відбулось раніше ніж минуло 10 секунд - нічого не відбувається
+
+const divForMakeMoney = document.createElement('div');
+const time = timeOfVisit.getTime();
+const styleDivMoney = {
+    width: '300px', backgroundColor: 'yellow', margin: '10px',
+    color: 'blue', textAlign: 'center', fontSize: '50px', clear: 'both'
+};
+Object.assign(divForMakeMoney.style, styleDivMoney);
+
+document.body.appendChild(divForMakeMoney);
+
+if (localStorage.getItem('time') === null) {
+    divForMakeMoney.innerText = '100гр';
+    localStorage.setItem('time', time.toString());
+    localStorage.setItem('money', '100'); 
+} else if ((time - +localStorage.getItem('time')) > 10000) {
+    const money = +localStorage.getItem('money') + 10;
+    divForMakeMoney.innerText = money + 'гр';
+    localStorage.setItem('money', money.toString());
+    localStorage.setItem('time', time.toString());
+} else {
+    divForMakeMoney.innerText = localStorage.getItem('money') + 'гр';
+    localStorage.setItem('time', time.toString());
+};
